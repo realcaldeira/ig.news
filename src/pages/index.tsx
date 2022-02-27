273d103269e89c9367696411a7210812bab6277e
Lucas Caldeira <mrrealcaldeira@gmail.com>
2022-02-26 21:18:38 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/jsx-key */
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>ig.news</title>
+      </Head>
+      <h1>Hello world !</h1>
+    </>
+  );
+}
